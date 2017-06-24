4a11564693d4cde4951ec1bdd58fb9cc665c2c94
aaronev <aarone.villanueva@gmail.com>
2017-06-24 11:46:42 -0700

    building database info

---
+++ models/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  artist VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  image VARCHAR(255),
+  timestamp TIMESTAMP DEFAULT current_timestamp
+);
+
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users,
+  album_id INTEGER REFERENCES albums,
+  review VARCHAR(255) NOT NULL,
+  timestamp TIMESTAMP DEFAULT current_timestamp
+);
