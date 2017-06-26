31e9c511d2cc4795e975274d0199bccfac4d122a
aaronev <aarone.villanueva@gmail.com>
2017-06-26 08:28:02 -0700

    got homepage to render correctly

--- app.js
+++ app.js
@@ -11,10 +11,17 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 //Helpers
 
-nonUserRenders = (res, user, albums, reviews, signin, signup) => {
-  return (res.render('body', {
+var userSession;
+
+// session = () => {
+
+// }
+
+
+nonUserRenders = ( res, users, albums, reviews, signin, signup ) => {
+  return ( res.render( 'body', {
     auth: null,
-    user: user,
+    users: users,
     albums: albums,
     reviews: reviews,
     signin: signin,
@@ -24,36 +31,48 @@ nonUserRenders = (res, user, albums, reviews, signin, signup) => {
 
 //Non-Users
 
-app.get('/', (req, res, next) => {
-  db.getAlbums()
-  .then(albums => {
-    db.getReviews()
-    .then(reviews => {
-      nonUserRenders(res, null, albums, reviews, null, null)
+app.get( '/', ( req, res, next ) => {
+  db.getAllInfo('users')
+  .then( users => {
+    db.getAllInfo('albums')
+    .then( albums => {
+      db.getAllInfo('reviews')
+      .then( reviews => {
+        nonUserRenders( res, users, albums, reviews, null, null )
+      })
     })
   })
-  .catch(next)
+  .catch( next )
 })
 
-app.get('/albums/:id', (req, res, next) => {
+app.get( '/albums/:id', ( req, res, next ) => {
   db.getAlbumsByID(req.params.id)
-  .then( albums => {
-    db.getReviewsByAlbumID(req.params.id)
-    .then( reviews => {
-      nonUserRenders(res, null, albums, reviews, null)
+  .then( albums => { 
+    db.getAllInfoWithCondition( req.params.id )
+    .then( albumreviews => {
+      console.log(albumreviews)
+      nonUserRenders( res, null, albums, albumreviews, null )
     })
   })
-  .catch(next)
+  .catch( next )
 })
 
-app.get('/signin', (req, res, next) => {
-  nonUserRenders(res, null, null, null, 'yes', null)
+app.get( '/signin', ( req, res, next ) => {
+  nonUserRenders( res, null, null, null, 'yes', null )
 })
 
-app.get('/signup', (req, res, next) => {
-  nonUserRenders(res, null, null, null, null, 'yes')
+app.get( '/signup', ( req, res, next ) => {
+  nonUserRenders( res, null, null, null, null, 'yes' )
 })
 
+app.post( '/email-availabilty', ( req, res, next ) => {
+  const { email, password, name } = req.body
+
+})
+
+app.post( '/verify-user', ( req, res, next ) => {
+  
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
--- models/queries.js
+++ models/queries.js
@@ -3,25 +3,15 @@ const dbName = 'vinyl'
 const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
 const db = pgp(connectionString)
 
-const getAlbums = function() {
-  return db.any("SELECT * FROM albums;")
+const getAllInfo = function(table) {
+  return db.any(`SELECT * FROM ${table};`)
 }
 
-const getAlbumsByID = function(id) {
-  return db.any("SELECT * FROM albums WHERE id = $1;", [id])
-}
-
-const getReviews = function() {
-  return db.any("SELECT * FROM reviews ORDER BY timestamp;")
-}
-
-const getReviewsByAlbumID = function(id) {
-  return db.any("SELECT * FROM reviews WHERE album_id = $1 ORDER BY timestamp;", [id])
+const getAllInfoByID = function(table, id) {
+  return db.any(`SELECT * FROM $1 WHERE id = $2;`, [table, id])
 }
 
 module.exports = {
- getAlbums,
- getAlbumsByID,
- getReviews,
- getReviewsByAlbumID,
+  getAllInfo,
+  getAllInfoByID
 }
